48203a57fdce180d1239ff8057b120d9a2bc0a38
MADANA-GS <madangsnaik@gmail.com>
2025-04-20 23:56:59 +0530

    categotry card was added

---
+++ client/src/Components/CategoryCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CategoryCard = () => {
+  return <div className="flex flex-col items-center">
+    <div className="overflow-hidden"> 
+    <img src="w-full h-full " alt="" />
+    
+    </div>
+
+  </div>;
+};
+
+export default CategoryCard;
